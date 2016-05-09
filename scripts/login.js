3bc2d1ba83f3cee9b06ba0df1be73e484f39e6cc
TellH <570495627@qq.com>
2016-05-09 22:07:13 +0800

    Initial commit

---
+++ scripts/login.js
@@ -0,0 +1,61 @@
+/**
+ * Created by tlh on 2016/5/8.
+ */
+$(document).ready(function () {
+    $("#btn_login").click(function () {
+        if (!validateForm($("#form_login")[0])) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "http://tanlehua.top/ReportSystem/account/login.do",
+            dataType: 'json',
+            data: {
+                userId: $("#login-id").val(),
+                password: $("#login-pass").val(),
+                identity: $("#select_identity").val()
+            },
+            success: function (data) {
+                if (data.result == "failed") {
+                    alert(data.msg);
+                } else if (data.result == "success") {
+                    alert(data.msg);
+                    if (data.identity == 0) {
+                        window.location.href = "student_home.html?userId=" + $("#login-id").val() + "identity=" + data.identity;
+                    }
+                    else if (data.identity == 1){
+                        window.location.href = "teacher_home.html?userId=" + $("#login-id").val() + "identity=" + data.identity;
+                    }
+                }
+            },
+            error: function (jqXHR) {
+                alert("似乎出现了些小问题,错误码：" + jqXHR.status);
+            }
+        })
+    });
+});
+function isFilled(field) {
+    if (field.value.length < 1 || field.value == field.defaultValue) {
+        return false;
+    } else {
+        return true;
+    }
+}
+function validateForm(whichFrom) {
+    for (var i = 0; i < whichFrom.elements.length; i++) {
+        var element = whichFrom.elements[i];
+        if (element.className.indexOf("required") != -1) {
+            if (!isFilled(element)) {
+                var id = element.id;
+                if (element.name == "用户ID号") $('#login-id').tooltip('show');
+                else $('#login-pass').tooltip('show');
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+$(function () {
+    $('[data-toggle=tooltip]').tooltip();
+});
