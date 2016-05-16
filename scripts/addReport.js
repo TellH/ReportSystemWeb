64fc99b4812544b06a4974744d1189de8dc5484c
TellH <570495627@qq.com>
2016-05-16 18:24:03 +0800

    完成了老师布置实验报告的表单设计

---
+++ scripts/addReport.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tlh on 2016/5/16.
+ */
+$(document).ready(function () {
+    $('#deadline').datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true,
+        todayHighlight: true
+    });
+});
